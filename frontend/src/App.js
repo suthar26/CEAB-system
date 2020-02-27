223bdd6a65409e4e449cfdf64ea0f4d69f3d1c56
Steven Pavlatos <100590313@oncampus.local>
2020-02-26 20:34:17 -0500

    Added Instructor Info page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CourseSyllabus from './CourseSyllabus';
+import InstrcutorInfo from './InstructorInfo';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <CourseSyllabus />
+        <InstrcutorInfo />
       </header>
     </div>
   );
