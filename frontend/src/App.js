bc33736374b23de70f172ffa7936affe2fd951bb
Savan Patel <savan.patel@dcmail.ca>
2020-03-04 23:08:27 -0500

    added new table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import CourseSyllabus from './CourseSyllabus';
 import InstrcutorInfo from './InstructorInfo';
+import Table3_1_2 from './Table3_1_2';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+const divStyle = {
+padding: '20px'};
   return (
     <div className="App">
-        <CourseSyllabus />
-        <InstrcutorInfo />
-      </header>
+        < CourseSyllabus style={divStyle}/>
+        <  InstrcutorInfo style={divStyle}/>
+        <  Table3_1_2 style={divStyle}/>
     </div>
   );
 }
