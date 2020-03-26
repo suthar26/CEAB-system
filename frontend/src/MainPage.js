3d37c99dce58b316966161654970f88b2a7d387b
parth suthar <parth1926@gmail.com>
2020-03-26 19:14:38 -0400

    added debug lines

--- frontend/src/MainPage.js
+++ frontend/src/MainPage.js
@@ -10,28 +10,27 @@ export default class MainPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            courseID : '',
+            courseID: '',
             instructorDetails: '',
             courseCode: ''
         };
-        this.textInput = React.createRef(); 
+        this.textInput = React.createRef();
 
     }
-    componentDidMount(){
+    componentDidMount() {
     }
     handleChange = e => this.setState({ [e.target.name]: e.target.value });
 
     loadSyllabus = () => {
-        const search = this.state.courseID; 
-        console.log(this.state.courseID);       
-        if(this.state.courseID != null){
+        console.log(this.state.courseID);
+        if (this.state.courseID != null) {
             fetch('/api/loadSyllabus')
                 .then((data) => data.json())
                 .then((res) => this.props.changeData(res.data))
                 .then(this.props.changeTab('CourseSyllabus'))
                 .then(console.log(this.props.getData));
         }
-        else{
+        else {
             this.props.changeData(null)
             this.props.changeTab('CourseSyllabus')
         }
@@ -39,27 +38,28 @@ export default class MainPage extends React.Component {
     };
 
     loadCourse = () => {
-        if(this.state.courseCode != null){
+        console.log(this.state.courseCode);
+        if (this.state.courseCode != null) {
             fetch('/api/loadCourse')
                 .then((data) => data.json())
                 .then((res) => this.props.changeData(res.data))
                 .then(console.log(this.props.getData));
         }
-        else{
+        else {
             this.props.changeData(null)
         }
 
     };
 
     loadTable = (search) => {
-        if(search != null){
+        if (search != null) {
             fetch('/api/loadTable')
                 .then((data) => data.json())
                 .then((res) => this.props.changeData(res.data))
                 .then(this.props.changeTab('Table3_1_2'))
                 .then(console.log(this.props.getData));
         }
-        else{
+        else {
             this.props.changeData(null)
             this.props.changeTab('Table3_1_2')
         }
@@ -67,14 +67,15 @@ export default class MainPage extends React.Component {
     };
 
     loadInstructor = () => {
-        if(this.state.instructorDetails != null){
+        console.log(this.state.instructorDetails);
+        if (this.state.instructorDetails != null) {
             fetch('/api/loadInstructor')
                 .then((data) => data.json())
                 .then((res) => this.props.changeData(res.data))
                 .then(this.props.changeTab('InstructorInfo'))
                 .then(console.log(this.props.getData));
         }
-        else{
+        else {
             this.props.changeData(null)
             this.props.changeTab('InstructorInfo')
         }
@@ -86,57 +87,57 @@ export default class MainPage extends React.Component {
             <div class="container" >
                 <h1>Course Syllabus </h1>
                 <InputGroup controlId="formControlsSelect" >
-                    <FormControl 
-                    name="courseID"
-                    onChange={this.handleChange}
-                    placeholder="Course ID"
-                    aria-label="Course ID"
-                    aria-describedby="basic-addon2"
+                    <FormControl
+                        name="courseID"
+                        onChange={this.handleChange}
+                        placeholder="Course ID"
+                        aria-label="Course ID"
+                        aria-describedby="basic-addon2"
                     />
                     <InputGroup.Append>
-                    <Button variant="primary" onClick={() => this.loadSyllabus()}>Load Course</Button>
-                    <Button variant="primary" onClick={() => this.loadSyllabus()}>New Course</Button>
+                        <Button variant="primary" onClick={() => this.loadSyllabus()}>Load Course</Button>
+                        <Button variant="primary" onClick={() => this.loadSyllabus()}>New Course</Button>
                     </InputGroup.Append>
                 </InputGroup>
 
                 <h1>Table 3.1.2</h1>
                 <InputGroup>
                     <InputGroup.Append>
-                    <Button variant="primary" onClick={() => this.loadTable('Table3_1_2')}>Load Course</Button>
-                    <Button variant="primary" onClick={() => this.loadTable(null)}>New Course</Button>
+                        <Button variant="primary" onClick={() => this.loadTable('Table3_1_2')}>Load Course</Button>
+                        <Button variant="primary" onClick={() => this.loadTable(null)}>New Course</Button>
                     </InputGroup.Append>
                 </InputGroup>
 
-                
+
                 <h1>Instructor Info </h1>
                 <InputGroup>
                     <FormControl
-                    name="instructorDetails"
-                    onChange={this.handleChange}                    
-                    placeholder="Instructor First & Last Name"
-                    aria-label="Instructor First & Last Name"
-                    aria-describedby="basic-addon2"
+                        name="instructorDetails"
+                        onChange={this.handleChange}
+                        placeholder="Instructor First & Last Name"
+                        aria-label="Instructor First & Last Name"
+                        aria-describedby="basic-addon2"
                     />
                     <InputGroup.Append>
-                    <Button variant="primary" onClick={() => this.loadInstructor()}>Load Course</Button>
-                    <Button variant="primary" onClick={() => this.loadInstructor()}>New Course</Button>
+                        <Button variant="primary" onClick={() => this.loadInstructor()}>Load Course</Button>
+                        <Button variant="primary" onClick={() => this.loadInstructor()}>New Course</Button>
                     </InputGroup.Append>
                 </InputGroup>
 
                 <h1>Upload Files</h1>
-                <input type="file" name="file" onChange={this.onChangeHandler}/>
+                <input type="file" name="file" onChange={this.onChangeHandler} />
 
                 <h1>Download Files </h1>
                 <InputGroup>
                     <FormControl
-                    name="courseCode"
-                    onChange={this.handleChange}                                        
-                    placeholder="Course Code"
-                    aria-label="Course Code"
-                    aria-describedby="basic-addon2"
+                        name="courseCode"
+                        onChange={this.handleChange}
+                        placeholder="Course Code"
+                        aria-label="Course Code"
+                        aria-describedby="basic-addon2"
                     />
                     <InputGroup.Append>
-                    <Button variant="primary" onClick={() => this.loadCourse()}>New Course</Button>
+                        <Button variant="primary" onClick={() => this.loadCourse()}>New Course</Button>
                     </InputGroup.Append>
                 </InputGroup>
             </div>
@@ -147,5 +148,6 @@ export default class MainPage extends React.Component {
 
 }
 
-        const divStyle = {
-        padding: '20px'};
+const divStyle = {
+    padding: '20px'
+};
